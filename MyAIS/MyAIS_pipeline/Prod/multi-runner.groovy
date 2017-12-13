136c1269b543afe370575f0efe0ec0cdf6082e88
AtlasArcadia <Atlas.ArcadiaCom@gmail.com>
2017-12-13 10:21:48 +0700

    commit by jenkins

--- MyAIS/MyAIS_pipeline/Prod/multi-runner.groovy
+++ MyAIS/MyAIS_pipeline/Prod/multi-runner.groovy
@@ -33,12 +33,12 @@ class Runner implements Serializable{
 	boolean Quickmenu_Search_Postpaid = false
 	boolean TopUpAndPayment = false
 	boolean PaymentTopUp = false
-	boolean CopyLogFromExecutorA = false
-	boolean CopyLogFromExecutorB = false
-	boolean AllReportMultiExecutors = false
+	boolean CopyLogFromExecutorA = true
+	boolean CopyLogFromExecutorB = true
+	boolean AllReportMultiExecutors = true
 	boolean ExportToReportExcel = false
-	boolean ExportToReportExcelMultiExecutors = false
-	boolean All = true
+	boolean ExportToReportExcelMultiExecutors = true
+	boolean All = false
 	boolean IsSmoke = false
 	boolean IsSanity = false
 	boolean activeFalse = false
